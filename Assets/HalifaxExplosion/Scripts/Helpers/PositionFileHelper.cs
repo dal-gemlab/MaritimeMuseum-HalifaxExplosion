d55c308e7b39d312c417ff879b74645099840789
Juliano Franz <jmfranz@users.noreply.github.com>
2017-10-05 23:32:22 -0300

    Added new models + Save/Load 100% (?)

--- Assets/HalifaxExplosion/Scripts/Helpers/PositionFileHelper.cs
+++ Assets/HalifaxExplosion/Scripts/Helpers/PositionFileHelper.cs
@@ -19,38 +19,13 @@ public static class PositionFileHelper
 {
     public static List<Transform> GetRelativePositions(string filename)
     {
-        
-        Stream stream = null;
-#if WINDOWS_UWP
+        List<Transform> storedTransforms = new List<Transform>();
+#if UNITY_EDITOR
+        UnityEditor.AssetDatabase.Refresh();
 
-        Task fileTask = new Task(
-            async () =>
-            {
-                StorageFolder sF = ApplicationData.Current.LocalFolder;
-                StorageFile posFile = await sF.GetFileAsync(filename);
-                var acessStream = await posFile.OpenReadAsync();
-                stream = acessStream.AsStreamForRead();
-            });
-        fileTask.Start();
-        fileTask.Wait();
-#else
-        try
-        {
-            stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+        TextAsset posFile = (TextAsset)Resources.Load(filename, typeof(TextAsset));
 
-        }
-        catch (FileNotFoundException e)
-        {
-            Debug.LogError("File not found!");
-            return null;
-
-        }
-#endif
-        if (stream == null)
-            return null;
-
-        StreamReader sr = new StreamReader(stream);
-        string json = sr.ReadToEnd();
+        string json = posFile.text;
         List<storeObject> objs = new List<storeObject>();
         foreach(var line in json.Split('\n'))
         {
@@ -61,12 +36,6 @@ public static class PositionFileHelper
 
 
         
-#if UNITY_WP8 || UNITY_WP8_1 || UNITY_WSA || UNITY_WSA_8_0 || UNITY_WSA_8_1 || UNITY_WSA_10_0
-        stream.Dispose();
-#else
-        stream.Close();
-#endif
-        List<Transform> storedTransforms = new List<Transform>();
         foreach (storeObject obj in objs)
         {
             GameObject g = new GameObject();
@@ -76,9 +45,45 @@ public static class PositionFileHelper
             UnityEngine.MonoBehaviour.Destroy(g);
 
         }
+#else
+
+        var objs = loadFileHolo(filename).Result;
+        foreach (storeObject obj in objs)
+        {
+            GameObject g = new GameObject();
+            g.transform.position = new Vector3(obj.position[0], obj.position[1], obj.position[2]);
+            g.transform.rotation = new Quaternion(obj.rotation[0], obj.rotation[1], obj.rotation[2], obj.rotation[3]);
+            storedTransforms.Add(g.transform);
+            UnityEngine.MonoBehaviour.Destroy(g);
+
+        }
+#endif
 
         return storedTransforms;
         
+    }
+
+    public static async Task<List<storeObject>> loadFileHolo(string filename)
+    {
+        Stream stream = null;
+        List<storeObject> objs = new List<storeObject>();
+        StorageFolder sF = ApplicationData.Current.LocalFolder;
+        StorageFile posFile = await sF.GetFileAsync(filename);
+        Debug.Log(sF.Path + "  " + sF.Name);
+        stream = await posFile.OpenStreamForWriteAsync();
+        StreamReader s = new StreamReader(stream);
+        string json = await s.ReadToEndAsync();
+
+        foreach (var line in json.Split('\n'))
+        {
+            //Here could be >1...
+            if (line.Length > 3)
+                objs.Add(JsonUtility.FromJson<storeObject>(line.ToString()));
+        }
+
+
+        stream.Dispose();
+        return objs;
     }
 
     public static bool SaveRelativePositions(List<Transform> transforms, string filename)
@@ -99,36 +104,33 @@ public static class PositionFileHelper
             serList.Add(obj);
         }
 
-
-#if WINDOWS_UWP
+        
+        string jsonTransforms = "";
+        foreach (var obj in serList)
+            jsonTransforms += JsonUtility.ToJson(obj) + '\n';
+#if UNITY_EDITOR
+        File.WriteAllText(Application.dataPath + "/Resources/" + filename + ".txt", jsonTransforms);
+#else
         Stream stream = null;
         Task fileTask = new Task(
             async () =>
             {
                 StorageFolder sF = ApplicationData.Current.LocalFolder;
-                StorageFile posFile = await sF.CreateFileAsync(filename);
+                StorageFile posFile = await sF.CreateFileAsync(filename,CreationCollisionOption.ReplaceExisting);
+                Debug.Log(sF.Path + "  " + sF.Name);
                 stream = await posFile.OpenStreamForWriteAsync();
+                StreamWriter s = new StreamWriter(stream);
+                await s.WriteAsync(jsonTransforms.ToCharArray());
+                s.Flush();
+                stream.Dispose();
             });
         fileTask.Start();
         fileTask.Wait();
-        StreamWriter s = new StreamWriter(stream);
-        string jsonTransforms = "";
-        foreach (var obj in serList)
-            jsonTransforms += JsonUtility.ToJson(obj) + '\n';
-        s.Write(jsonTransforms.ToCharArray());
-        s.Flush();
-        stream.Dispose();
 
-#else
-        Stream stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None);
-        StreamWriter s = new StreamWriter(stream);
-        string jsonTransforms = "";
-        foreach (var obj in serList)
-            jsonTransforms += JsonUtility.ToJson(obj) + '\n';
-        s.Write(jsonTransforms.ToCharArray());
-        s.Flush();
-        stream.Close();
+        
 #endif
+
+
         return true;
     }
 
