a740623221b9f345455d98dc9aa0f7ef7a7cfeb8
Juliano Franz <jufranz@gmail.com>
2018-08-09 08:48:37 -0300

    Fooling around

---
+++ HoloLensProject/Assets/HalifaxExplosion/Test/PositionAndRotateCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionAndRotateCamera : MonoBehaviour
+{
+
+    public Vector3 Position;
+    public Quaternion Rotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = Position;
+	    transform.rotation = Rotation;
+	}
+}
