6288e3261da73ff38f427e185736dae6abb2a4ad
Juliano Franz <jufranz@gmail.com>
2018-11-29 08:16:48 -0400

    Camera sticks

---
+++ HoloLensProject/Assets/HalifaxExplosion/DataAnalysisStuff/LaserPlacer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPlacer : MonoBehaviour {
+
+    public Vector3 Position;
+    public Quaternion Rotation;
+    public string LogLine;
+
+    private void Update()
+    {
+
+        var values = LogLine.Split(',');
+        if(values.Length < 4)
+            return;;
+        Position = new Vector3(
+            float.Parse(values[0]),
+            float.Parse(values[1]),
+            float.Parse(values[2])
+            );
+
+        Rotation = new Quaternion(
+            float.Parse(values[3]),
+            float.Parse(values[4]),
+            float.Parse(values[5]),
+            float.Parse(values[6])
+            );
+
+
+        transform.position = Position;
+        transform.rotation = Rotation;
+
+        transform.name = values[7];
+    }
+}
